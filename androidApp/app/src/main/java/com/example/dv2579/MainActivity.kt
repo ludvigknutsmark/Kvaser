85f028f0cb121ebeef3bd63fd78a1fb60b3456cc
Hampus Bengtsson <hampusbengtsson@live.se>
2019-12-23 16:56:09 +0100

    new code to solve ssl_pinning

--- androidApp/app/src/main/java/com/example/dv2579/MainActivity.kt
+++ androidApp/app/src/main/java/com/example/dv2579/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.dv2579
 
+
 import android.os.AsyncTask
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,9 +10,11 @@ import org.json.JSONObject
 import java.io.BufferedInputStream
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
-import javax.net.ssl.HttpsURLConnection
+import okhttp3.CertificatePinner
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,30 +22,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GetWeatherTask(textView10).execute()
+        sslPinning(textView2).execute()
     }
 
-    class GetWeatherTask(textView: TextView) : AsyncTask<Unit, Unit, String>() {
+    class sslPinning(textView: TextView) : AsyncTask<Unit, Unit, String>() {
 
         val innerTextView: TextView? = textView
 
         override fun doInBackground(vararg params: Unit?): String? {
-            val url = URL("https://collinstuart.github.io/posts.json")
-            val httpsClient = url.openConnection() as HttpsURLConnection
-            if (httpsClient.responseCode == HttpsURLConnection.HTTP_OK) {
-                try {
-                    val stream = BufferedInputStream(httpsClient.inputStream)
-                    val data: String = readStream(inputStream = stream)
-                    return data
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                } finally {
-                    httpsClient.disconnect()
-                }
-            } else {
-                println("ERROR ${httpsClient.responseCode}")
+            val hostname = "google.se"
+            try {
+                val certificatePinner = CertificatePinner.Builder()
+                    .add(hostname, "sha256/JBruFRoov64D6cb+fs3Qcp7UzWVHKOnr/WoApKUnwLk=")
+                    .build()
+                val client: OkHttpClient = OkHttpClient.Builder()
+                    .certificatePinner(certificatePinner)
+                    .build()
+
+                val request: Request = Request.Builder()
+                    .url("https://$hostname")
+                    .build()
+                client.newCall(request).execute()
             }
-            return null
+            catch (e: Exception){
+                return """{"error": "SSL pinning failed"}"""
+            }
+            return """{"success": "SSL pinning done!"}"""
+
         }
 
         fun readStream(inputStream: BufferedInputStream): String {
